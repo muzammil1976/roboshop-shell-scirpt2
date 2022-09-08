8465e54d8148987c30af8f3ede79d2d1fafd6015
muzammil1976 <muzammil.shah@gmail.com>
2022-09-08 05:03:09 -0500

    demo using mongodb

--- components/cart.sh
+++ components/cart.sh
@@ -1,7 +1,6 @@
 url -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
 rm -rf cart
