1129f258faa4339da31435c9722a0984cc308f80
muzammil1976 <muzammil.shah@gmail.com>
2022-09-11 09:14:26 -0500

    push 1-5

--- components/shipping.sh
+++ components/shipping.sh
@@ -8,7 +8,7 @@ mv shipping-main shipping
 cd shipping
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-sed -i -e 's/CARTENDPOINT/cart.robohsop.internal1976/' -e 's/DBHOST/mysql.roboshop.internal1976/' /home/roboshop/shipping/sysytemd.service
+sed -i -e 's/CARTENDPOINT/cart.roboshop.internal1976/' -e 's/DBHOST/mysql.roboshop.internal1976/' /home/roboshop/shipping/sysytemd.service
 
 mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
