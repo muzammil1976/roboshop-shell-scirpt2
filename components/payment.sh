6c766a492ffbba62bbbb09e69d0837e8dff27495
muzammil1976 <muzammil.shah@gmail.com>
2022-09-11 13:11:32 -0500

    push 1-5

--- components/payment.sh
+++ components/payment.sh
@@ -5,6 +5,9 @@ rm -rf payment
 curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
 unzip /tmp/payment.zip
 mv payment-main payment
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+
 mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
