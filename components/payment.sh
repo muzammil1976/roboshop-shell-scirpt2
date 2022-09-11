7f22f4d278bc2d9cedce07733a0cbe6a2785c4b2
muzammil1976 <muzammil.shah@gmail.com>
2022-09-11 10:22:19 -0500

    push 1-5

--- components/payment.sh
+++ components/payment.sh
@@ -1,1 +1,11 @@
-echo install payment component
+yum install python36 gcc python3-devel -y
+seradd roboshop
+cd /home/roboshop
+rm -rf payment
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+mv payment-main payment
+mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
+systemctl daemon-reload
+systemctl enable payment
+systemctl start payment
