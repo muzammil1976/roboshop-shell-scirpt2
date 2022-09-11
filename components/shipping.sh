e15e7b081b2071c16f11c3886b81998ff5e2ae9b
muzammil1976 <muzammil.shah@gmail.com>
2022-09-11 09:04:00 -0500

    push 1-5

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,1 +1,16 @@
-echo install shipping component
+yum install maven -y
+useradd roboshop
+cd /home/roboshop
+rm -rf shipping
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+unzip /tmp/shipping.zip
+mv shipping-main shipping
+cd shipping
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+sed -i -e 's/CARTENDPOINT/cart.robohsop.internal1976/' -e 's/DBHOST/mysql.roboshop.internal1976/' /home/roboshop/shipping/sysytemd.service
+
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
