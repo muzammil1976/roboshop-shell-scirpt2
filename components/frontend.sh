a55c1ed32cd7c87de77d93d5b145d66dc23215d6
muzammil1976 <muzammil.shah@gmail.com>
2022-09-17 07:42:25 -0500

    update roboshop

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo You are non root user
-    echo you can run this script as root user or with sudo
-    exit 1
-fi
+source components/common.sh
+
+CHECK_ROOT
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
