5e77131254e2c291226ba0b70a4b3c6f9bc4dded
muzammil1976 <muzammil.shah@gmail.com>
2022-09-17 07:46:32 -0500

    update roboshop

--- components/common.sh
+++ components/common.sh
@@ -1,7 +1,7 @@
 CHECK_ROOT() {
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ]; then
-      echo You are non root user
+      echo i have sone with autism and he is very aggrassive
       echo you can run this script as root user or with sudo
       exit 1
   fi
