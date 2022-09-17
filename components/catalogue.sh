8c1fda20359f4ff087ac08b564bdef15b20c4e46
muzammil1976 <muzammil.shah@gmail.com>
2022-09-17 07:39:39 -0500

    update roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,9 +1,6 @@
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo You are non root user
-    echo you can run this script as root user or with sudo
-    exit 1
-fi
+source components/common.sh
+
+CHECK_ROOT
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 
