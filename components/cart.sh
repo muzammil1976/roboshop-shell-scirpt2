1419411a759c1e18c37619bbb66b0b2bd77b5097
muzammil1976 <muzammil.shah@gmail.com>
2022-09-17 07:36:43 -0500

    update roboshop

--- components/cart.sh
+++ components/cart.sh
@@ -1,11 +1,4 @@
-CHECK_ROOT() {
-  USER_ID=$(id -u)
-  if [ $USER_ID -ne 0 ]; then
-      echo You are non root user
-      echo you can run this script as root user or with sudo
-      exit 1
-  fi
-}
+source components/common.sh
 
 CHECK_ROOT
 
