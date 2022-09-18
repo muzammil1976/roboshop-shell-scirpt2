4b08ebd2a44961c7ae99ee48c0e642e82cb9689b
muzammil1976 <muzammil.shah@gmail.com>
2022-09-18 13:30:57 -0500

    update roboshop

--- components/cart.sh
+++ components/cart.sh
@@ -2,8 +2,36 @@ source components/common.sh
 
 CHECK_ROOT
 
-echo "setting up NodeJS YUM Repo is "
+PRINT "setting up NodeJS YUM Repo is "
 url -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Installing NodeJS"
+yum install nodejs -y &>>${LOG}
+CHECK_STAT $?
+
+echo "Creating Application User"
+useradd roboshop &>>${LOG}
+CHECK_STAT $?
+
+echo "Downloading Cart Content"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${log}
+CHECK_STAT $?
+
+cd /home/roboshop
+
+echo "Remove old content"
+rm -rf cart &>>${LOG}
+CHECK_STAT $?
+
+echo "Exrtact old content"
+unzip /tmp/cart.zip &>>${LOG}
+CHECK_STAT $?
+
+mv cart-main cart
+cd cart
+
+PRINT "install NodeJS
 if [ $? -ne 0 ]; then
   echo FAILED
   exit 2
